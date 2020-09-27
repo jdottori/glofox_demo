ab087ee74eafb519a39a8b4368c38ee7a6bffb55
Javier Dottori <javier.dottori@gmail.com>
2020-09-27 19:27:10 -0300

    Initial commit

---
+++ src/main/java/com/jdottori/example/glofox/GlofoxApplication.java
@@ -0,0 +1,13 @@
+package com.jdottori.example.glofox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlofoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlofoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdottori/example/glofox/GlofoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdottori.example.glofox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlofoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
